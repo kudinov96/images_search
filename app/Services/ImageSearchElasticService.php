c405128cb0d89cb20c87421a11e77e1e867a11ec
Ilya Kudinov <ilya.kudinov96@yandex.ru>
2024-12-11 15:18:43 +0300

    upd

--- app/Services/ImageSearchElasticService.php
+++ app/Services/ImageSearchElasticService.php
@@ -16,7 +16,7 @@ class ImageSearchElasticService
         $this->client = ClientBuilder::create()
             ->setHosts(config('elasticsearch.hosts'))
             ->setBasicAuthentication('elastic', config('elasticsearch.password'))
-            ->setSSLCert("/etc/elasticsearch/certs/http_ca.crt")
+            ->setSSLCert("/home/ikudinov/sis.pics/elastic.crt")
             ->build();
     }
 
